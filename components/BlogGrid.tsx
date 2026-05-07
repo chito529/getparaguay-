6c365bda6a07ff0d4ac02ae8b220b08242ffa55b
chito529 <juanalmiron529@gmail.com>
2026-05-07 09:33:35 -0300

    Update BlogGrid.tsx

--- components/BlogGrid.tsx
+++ components/BlogGrid.tsx
@@ -14,7 +14,7 @@ const posts: BlogPost[] = [
     title: "The Quiet Rise of Asunción Real Estate",
     excerpt: "Why savvy international investors are turning their eyes to the growing skyline of Paraguay's capital.",
     category: "Invest",
-    image: "https://picsum.photos/600/400?random=1",
+    image: "https://images.unsplash.com/photo-1646837599653-e39f2be8a310?q=80&w=2832&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     id: 2,
